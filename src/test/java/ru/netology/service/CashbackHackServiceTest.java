5f00834a1d1d7e8978de07c8c7095dad9599d6b9
Alena Kuimova <alvikui@yandex.ru>
2023-09-19 22:51:22 +0600

    Change TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -20,6 +20,15 @@ public class CashbackHackServiceTest {
     public void GreaterThanBoundary() {
         CashbackHackService cashbackService = new CashbackHackService();
         int amount = 1200;
+        int expected = 800;
+        int actual = cashbackService.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void EqualsBoundary() {
+        CashbackHackService cashbackService = new CashbackHackService();
+        int amount = 1000;
         int expected = 0;
         int actual = cashbackService.remain(amount);
         Assert.assertEquals(expected, actual);
