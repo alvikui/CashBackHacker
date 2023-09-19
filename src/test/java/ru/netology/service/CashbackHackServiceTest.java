4a2e80e4651e3bccd64ffe22a83cd7760bf86df1
Alena Kuimova <alvikui@yandex.ru>
2023-09-19 22:39:42 +0600

    Add TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,27 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void LessThanBoundary() {
+        CashbackHackService cashbackService = new CashbackHackService();
+        int amount = 500;
+        int expected = 500;
+        int actual = cashbackService.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void GreaterThanBoundary() {
+        CashbackHackService cashbackService = new CashbackHackService();
+        int amount = 1200;
+        int expected = 0;
+        int actual = cashbackService.remain(amount);
+        Assert.assertEquals(expected, actual);
+    }
+}
